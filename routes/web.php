c61d43f9897ee407b0b88e2462e9ea8004831f88
Yumihiki <nibu10.majik@gmail.com>
2019-10-05 11:31:46 +0900

    ルート定義

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,31 @@
 |
 */
 
+use App\Task;
+use Illuminate\Http\Request;
+
+/**
+ * 的中表示
+ */
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('points');
+});
+
+/**
+ * 的中追加
+ */
+Route::post('/point', function (Request $request) {
+    //
+});
+
+/**
+ * 的中編集
+ */
+
+/**
+ * 的中削除
+ */
+Route::delete('point/{point}', function (Point $point) {
+    //
 });
