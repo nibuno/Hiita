01925f3a363f945b91f5646380f8d8b5b66ca57d
Yumihiki <nibu10.majik@gmail.com>
2019-10-12 12:31:29 +0900

    app.nameの修正とログイン時にはHiitaクリック時にdashboardへ移動するように

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Hiita') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -35,9 +35,15 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
+                @guest 
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Hiita') }}
                 </a>
+                @else 
+                <a class="navbar-brand" href="{{ url('/dashboard') }}">
+                    {{ config('app.name', 'Hiita') }}
+                </a>
+                @endguest
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
