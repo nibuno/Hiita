824e16c8409e4d94c6af74dc7f7fdaddb643324f
Yumihiki <nibu10.majik@gmail.com>
2019-10-21 16:10:50 +0900

    pointsテーブルにdateカラムを追加

---
+++ database/migrations/2019_10_21_093644_add_date_to_points_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDateToPointsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('points', function (Blueprint $table) {
+            $table->date('date')->nullable($value = true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('points', function (Blueprint $table) {
+            //
+        });
+    }
+}
