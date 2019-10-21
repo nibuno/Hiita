1340cd9ebc5df2d1c757e08220f7c43a49edd3dd
Yumihiki <nibu10.majik@gmail.com>
2019-10-21 20:35:03 +0900

    カレンダーの生成

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -3,11 +3,71 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class CalendarController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $requestYm = $request->input('Ym');
+
+        // 実行した年月日を取得
+        $currentDays = new Carbon();
+        $currentYear = $currentDays->format('Y');
+        $currentMonth = $currentDays->format('m');
+        $currentDay = $currentDays->format('d');
+
+        if (!$request->has('Ym')) {
+            $year = Carbon::today()->format('Y');
+            $month = Carbon::today()->format('m');
+            $day = Carbon::today()->format('d');
+            $yearMonth = Carbon::today()->format('Ym');
+        } else {
+            $requestDay = new Carbon($requestYm);
+            $year = $requestDay->format('Y');
+            $month = $requestDay->format('m');
+            $day = $requestDay->format('d');
+            $yearMonth = $requestDay->format('Ym');
+        }
+
+        $weeks = [
+            '日' => 'sunday',
+            '月' => 'monday',
+            '火' => 'tuesday',
+            '水' => 'wednesday',
+            '木' => 'thursday',
+            '金' => 'friday',
+            '土' => 'saturday',
+          ];
+
+        /**
+         * https://crieit.net/posts/Laravel-Carbon
+         * こちらを利用させてもらう
+         */
+        $dateStr = sprintf('%04d-%02d-01', $year, $month);
+        $date = new Carbon($dateStr);
+
+        // カレンダーを四角形にするため、前月となる左上の隙間用のデータを入れるためずらす
+        $date->subDay($date->dayOfWeek);
+        // 同上。右下の隙間のための計算。
+        $count = 31 + $date->dayOfWeek;
+        $count = ceil($count / 7) * 7;
+        $dates = [];
+
+        for ($i = 0; $i < $count; $i++, $date->addDay()) {
+            // copyしないと全部同じオブジェクトを入れてしまうことになる
+            $dates[] = $date->copy();
+        }
+
+        return view('calendar', [
+            'dates' => $dates,
+            'year' => $year,
+            'month' => $month,
+            'day' => $day,
+            'currentYear' => $currentYear,
+            'currentMonth' => $currentMonth,
+            'currentDay' => $currentDay,
+            'weeks' => $weeks,
+        ]);
     }
 }
--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -2,4 +2,40 @@
 
 @section('content')
 
+<div class="container">
+    <div class="text-center">
+        {{ $year }} - {{ $month }}
+    </div>
+    <table class="table table-bordered">
+        <thead>
+        <tr>
+            @foreach ($weeks as $jaDayOfWeek => $enDayOfWeek)
+            <th class="{{ $enDayOfWeek }} text-center">{{ $jaDayOfWeek }}</th>
+            @endforeach
+        </tr>
+        </thead>
+        <tbody>
+        @foreach ($dates as $date)
+            @if ($date->dayOfWeek == 0)
+                <tr>
+            @endif
+                <td
+                    @if ($date->year != $currentYear || $date->month != $currentMonth)
+                        class="bg-secondary"
+                    
+                    @elseif ($date->year == $currentYear && $date->month == $currentMonth && $date->day == $currentDay)
+                        class="bg-primary"
+                    
+                    @endif
+                >
+                {{ $date->day }}
+                </td>
+            @if ($date->dayOfWeek == 6)
+                </tr>
+            @endif
+        @endforeach
+        </tbody>
+    </table>
+</div>
+
 @endsection
