6d0bf5ef345e942e80e2942ff212c80cb3bec07e
Yumihiki <nibu10.majik@gmail.com>
2019-11-09 17:36:55 +0900

    パスワード確認メッセージをplaceholder内に格納

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -54,10 +54,10 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right"></label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="確認のため2回入力してください">
                             </div>
                         </div>
 
